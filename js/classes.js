cf9a3028af13542bba744f5eb34c59d81edcfcb0
witcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-11-13 23:22:06 +0200

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -25,7 +25,7 @@ User.prototype.exit = function() {
 }
 
 const ivan = new User('Ivan', '28');
-const alex = new User('Steve', '31');
+const steve = new User('Steve', '31');
 
 ivan.hello();
 ivan.exit();
@@ -47,7 +47,7 @@ class User2 {
     }
 }
 
-const alex2 = new User2('alex stupid', 124);
+const steve2 = new User2('steve smart', 124);
 
 class Rectangle {
     constructor(height, width) {
